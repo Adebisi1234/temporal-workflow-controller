710bee525afd203b428c844ae02334500aacacb2
Tobiloba Adebisi <ti.adebisi@gmail.com>
2025-02-08 14:19:29 +0100

    fix: moving python to docker

--- flask/start.sh
+++ flask/start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/sh
 
-temporal server start-dev --headless --log-level fatal & gunicorn --worker-class=gevent --worker-connections=1000 --workers=5 app:app & python3 ./run_worker.py
+temporal server start-dev --headless --log-level error & gunicorn --worker-class=gevent --worker-connections=1000 --workers=5 app:app & python3 ./run_worker.py
